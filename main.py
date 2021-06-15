657b69c0c16c17f2c435ba5223c9fb64418a91b3
metanoia <meta3479@gmail.com>
2021-06-15 10:06:58 +0300

    ARMY not ARMIE

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from units_builder import UnitsBuilder
 
 
 def main():
-    armies = {UnitsBuilder(f"ARMIE_{army+1}", 5, 2)() for army in range(5)}
+    armies = {UnitsBuilder(f"ARMY_{army+1}", 5, 2)() for army in range(5)}
     game = Game(armies)
     game.run_game()
 
